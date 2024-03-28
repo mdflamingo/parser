98ef7f1eb4d16912fe3f6f581474e646df3879ee
Анастасия Искоскина <aiskoskina@yandex.ru>
2024-03-28 19:57:15 +0300

    new version

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
+import json
+import csv
 
-page = requests.get('https://www.agroviola.ru/collection/vegetativnye-cherenki-vesna-2022g')
 
-soup = BeautifulSoup(page.text, 'lxml')
+url = 'https://www.agroviola.ru/collection/albom-vse-sorta'
 
-product_cards = soup.find_all(class_="product-card-inner")
+headers = {
+    'Accept': 'text/css,*/*;q=0.1',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+}
 
-with open('flower.txt', 'w') as f:
-    for card in product_cards:
-        flower = card.text.strip().replace('Подробнее', '').replace('Товар отсутствует','').split('    ')
-        f.write(flower[-1] + '\n')
+req = requests.get(url, headers=headers)
+src = req.text
+
+with open('index.html', 'w') as file:
+    file.write(src)
+
+with open('index.html') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+
+all_products_href = soup.find_all(class_="product-link")
+
+all_products_dict = {}
+
+for item in all_products_href:
+    item_text = item.text
+    item_href = 'https://www.agroviola.ru/' + item.get('href')
+
+    all_products_dict[item_text.strip()] = item_href
+
+with open('all_categories_dict.json', 'w') as file:
+    json.dump(all_products_dict, file, indent=4, ensure_ascii=False)
+
+with open('all_categories_dict.json') as file:
+    all_categories = json.load(file)
+
+count = 0
+
+for category_name, category_href in all_categories.items():
+    result_string = re.sub(r'\d+', '', category_name)
+    category_name_final = result_string.replace('(Весна )', '').strip()
+
+    req = requests.get(url=category_href, headers=headers)
+    src = req.text
+
+    with open(f'data/{count}_{category_name_final}.html', 'w') as file:
+        file.write(src)
+
+    with open(f'data/{count}_{category_name_final}.html') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+    description = soup.find(class_='tab-block-inner editor').text.strip()
+
+    with open(f'data/flowers.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow((
+            category_name_final,
+            description
+        ))
+    count += 1
